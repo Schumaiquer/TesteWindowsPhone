89997bf0b4322645aa81600cbbe25b653bed86ab
Schumaiquer Souto <schumaiquer08@gmail.com>
2017-06-21 15:58:31 -0300

    servico

---
+++ ProjectWindowsPhone/Model/Repositorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Repositorio
+    {
+        public string nomeRepositorio { get; set; }
+        public string descricao { get; set; }
+        public string nomeAutor { get; set; }
+        public string FotoAutor { get; set; }
+        public string NumeroEstrela { get; set; }
+        public string NumeroForks { get; set; }
+
+    }
+}
---
+++ ProjectWindowsPhone/Service/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Service
+{
+   static class Client
+    {
+        private static HttpClient httpClient = new HttpClient();
+
+        public static async Task<Repositorio> GetRepositorio(string uri) {
+            Repositorio repositorio = null;
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+
+            if (response.IsSuccessStatusCode) {
+                repositorio = await response.Content.ReadAsAsync<Repositorio>();
+            }
+            return repositorio;
+        }
+
+        }
+ }
